e403dde6cdac34629fe1ab6adfef5e38b0826f5e
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-04-14 19:37:47 +0700

    Adjust Koopa speed

--- SuperMarioBros3/Koopa.h
+++ SuperMarioBros3/Koopa.h
@@ -2,7 +2,7 @@
 #include "Enemy.h"
 
 #define KOOPA_GRAVITY 0.002f
-#define KOOPA_WALKING_SPEED 0.03f
+#define KOOPA_WALKING_SPEED 0.05f
 #define KOOPA_SLIDING_SPEED 0.2f
 #define KOOPA_SHAKE_SPEED 0.1f
 
