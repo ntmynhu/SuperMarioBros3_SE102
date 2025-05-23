979452e9ae9189c9fc0c6db41705ec677feb4790
Nguyen Thi My Nhu <ntmn1105@gmail.com>
2025-05-23 08:38:44 +0700

    Add score effect when tailattack

--- SuperMarioBros3/Goomba.cpp
+++ SuperMarioBros3/Goomba.cpp
@@ -89,6 +89,10 @@ void CGoomba::TakeTailAttackDamage(float xMario)
 		else {
 			vx = ENEMY_DIE_UPSIDE_DOWN_VX;
 		}
+
+		CGame* game = CGame::GetInstance();
+		game->AddScore(100, x, y, this);
+
 		vy = -ENEMY_DIE_UPSIDE_DOWN_VY;
 		SetState(ENEMY_STATE_DIE);
 	}
